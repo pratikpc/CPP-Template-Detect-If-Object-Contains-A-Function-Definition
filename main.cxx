4bd6849f02bcdffd0016a0ec03136883db852183
Pratik <pratikc@live.co.uk>
2023-09-19 17:04:15 +0000

    feat(Code/Structs): add samples
    We will test the ones with the name Test

--- main.cxx
+++ main.cxx
@@ -1,5 +1,31 @@
 #include <iostream>
 
+struct ContainsFunction
+{
+    void Test(int x, int y)
+    {
+
+    }
+};
+
+struct ContainsFunctionWithDifferentParameters
+{
+    void Test(int x)
+    {
+
+    }
+};
+
+struct DoesNotContainTestFunction
+{
+    void DifferentFunction(int x)
+    {
+        
+    }
+};
+
+
+
 int main()
 {
     std::cout << "Executing code" << std::endl;
