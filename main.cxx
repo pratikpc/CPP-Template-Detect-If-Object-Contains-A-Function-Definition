44025e571f71abdeab8c8eb74e474d1bd48e6595
Pratik <pratikc@live.co.uk>
2023-09-19 17:26:33 +0000

    feat(Code/Compile Time Detector): add
    Using C++20 requires

--- main.cxx
+++ main.cxx
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <type_traits>
 
 struct ContainsFunction
 {
@@ -24,9 +25,51 @@ struct DoesNotContainTestFunction
     }
 };
 
+template<typename T>
+void TemplatedTestCaller(T t)
+{
+    /// @brief Thanks to C++20's Requires expressions,
+    /// this can be done very easily
+    /// The requires expression is able to check if the given code
+    /// with the given arguements will work or not
+    static constexpr auto const contains_test = requires(T t)
+    {
+        t.Test(1, 2);
+    };
 
+    /// @note The type of contains_test will be a boolean
+    static_assert(
+        std::is_same_v<
+            std::remove_cvref_t<decltype(contains_test)>,
+            bool
+        >
+    );
+
+    /// Take a decision at compile time whether we wish to call Test or do something else
+    /// contains_test will be true only for functions where Test function with 2 integers is present
+    if constexpr(contains_test)
+    {
+        t.Test(1, 2);
+        std::cout << "Contains Test function with the correct parameters" << std::endl;
+    }
+    else
+    {
+        std::cout << "Does not contain Test function with Correct Parameters" << std::endl;
+    }
+}
 
 int main()
 {
-    std::cout << "Executing code" << std::endl;
+    {
+        ContainsFunction c;
+        TemplatedTestCaller(c);
+    }
+    {
+        ContainsFunctionWithDifferentParameters c;
+        TemplatedTestCaller(c);
+    }
+    {
+        DoesNotContainTestFunction c;
+        TemplatedTestCaller(c);
+    }
 }
